eee97ba84c351404aeed0728f1cd91f1d507a2fc
Francesco <Francesco Allegrini>
2025-03-26 11:35:54 +0100

    Fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fraalle.github.io',
+  base: '/Webpage',
   integrations: [tailwind()],
 });
